ebaf54faeb37fc1658283feccb5233e76559914e
Bo Eriksson <bo.eriksson@kindredgrougroupp.com>
2017-07-14 10:28:09 +0200

    Colorcode js-snippet

--- src/main/js/findClosingBracket/findClosingBracket.js
+++ src/main/js/findClosingBracket/findClosingBracket.js
@@ -1,7 +1,7 @@
 /**
  *  Feed it a string and a start pos, and it will scan for brackets and return position of where the
  *  closing bracket is taking it down to the the same level it started at.
- *  
+ *
  *  Returns null if end of string is encountered first
  */
 export default findClosingBracket = (searchStr, start, level = 0) => {
